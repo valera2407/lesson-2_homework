beffd48aa703e3824e4ebabfba203de47a322bb6
Tod <valerafatkulin7@gmail.com>
2021-06-21 02:00:50 +0300

    hometask

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -3,6 +3,33 @@ package com.alevel;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        int numbers = 347693485;
+        int times = 9;
+        int num;
+        int i = 0;
+        while (true){
+            num = numbers % 10;
+            i = i * 10 + num;
+            numbers = numbers / 10;
+            if (i == 584396743) {
+                for (int x=0;x <= 8; x++) {
+                    num = i % 10;
+                    if (num % 2 == 0 && num % 3 == 0) {
+                        System.out.println("fizzbuzz");
+                    } else if (num % 2 == 0) {
+                        System.out.println("fizz");
+                    } else if (num % 3 == 0) {
+                        System.out.println("buzz");
+                    } else {
+                        System.out.println("Number is " + num);
+                    }
+                    i = i / 10;
+                }
+            }
+            times--;
+            if (times == 0)
+                return;
+        }
     }
 }
+//слева направо
