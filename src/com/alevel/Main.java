38f9b27b55e50d60021efd841791abccd40b41f4
Tod <valerafatkulin7@gmail.com>
2021-06-21 12:42:38 +0300

    hometask

--- src/com/alevel/Main.java
+++ src/com/alevel/Main.java
@@ -5,25 +5,43 @@ public class Main {
     public static void main(String[] args) {
         int numbers = 347693485;
         int num;
-        int i = 0;
-        while (numbers>0) {
-            num = numbers % 10;
-            i = i * 10 + num;
-            numbers = numbers / 10;
-        }
-        while (i>0){
-            num = i % 10;
-            if (num % 2 == 0 && num % 3 == 0) {
+        while (numbers>0){
+            num = numbers%10;
+            if (num%2==0 && num%3==0) {
                 System.out.println("fizzbuzz");
-            } else if (num % 2 == 0) {
+            }else if (num % 2 == 0) {
                 System.out.println("fizz");
-            } else if (num % 3 == 0) {
+            }else if (num % 3 == 0) {
                 System.out.println("buzz");
-            } else {
+            }else {
                 System.out.println("Number is " + num);
             }
-            i = i / 10;
+            numbers = numbers/10;
         }
+        System.out.println("Your task completed");
+        //справо налево
+        int beforeInvert = 347693485;
+        int numIvert;
+        int forIvert = 0;
+        while (beforeInvert>0) {
+            numIvert = beforeInvert % 10;
+            forIvert = forIvert * 10 + numIvert;
+            beforeInvert = beforeInvert / 10;
+        }
+        while (forIvert>0){
+            numIvert = forIvert % 10;
+            if (numIvert % 2 == 0 && numIvert % 3 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (numIvert % 2 == 0) {
+                System.out.println("fizz");
+            } else if (numIvert % 3 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println("Number is " + numIvert);
+            }
+            forIvert = forIvert / 10;
+        }
+        System.out.println("Okay, we did it");
+        //слева направо
     }
 }
-//слева направо
